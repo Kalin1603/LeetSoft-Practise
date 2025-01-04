6ee384de5f7b5671abbd278174a650c93c1cd174
Kalin1603 <kalintodorov03@gmail.com>
2025-01-04 12:46:45 +0200

    Created table User

--- Project/Event Management/Data/ApplicationDbContext.cs
+++ Project/Event Management/Data/ApplicationDbContext.cs
@@ -1,13 +1,16 @@
-﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+﻿using Event_Management.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
 namespace Event_Management.Data
 {
-    public class ApplicationDbContext : IdentityDbContext
+    public class ApplicationDbContext : IdentityDbContext<User>
     {
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
             : base(options)
         {
         }
+
+        public DbSet<User> Users { get; set; }
     }
 }
---
+++ Project/Event Management/Models/User.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
+
+namespace Event_Management.Models
+{
+    public class User : IdentityUser
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(50)]
+        public string FirstName { get; set; }
+
+        [Required]
+        [StringLength(50)]
+        public string LastName { get; set; }
+
+        [Required]
+        [StringLength(150)]
+        public string PhoneNumber { get; set; }
+
+        [Required]
+        [Range(10, 18)]
+        public int Age { get; set; }
+
+        public string Email { get; set; }
+
+        //public ICollection<Event> Events { get; set; }
+    }
+}
