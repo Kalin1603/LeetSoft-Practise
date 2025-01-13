29a03c54e53be3ab49e315fd84b00a38dd843726
YuroslavPM <yuroslavpm11@gmail.com>
2025-01-13 11:51:17 +0200

    Updated Front-end of Event View (CRUD) and updated Home View(Index)

---
+++ Project/Event Management/Data/Migrations/20250113092145_FinalYMigration.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Event_Management.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class FinalYMigration : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
